3d04680e3077b2ce52d22998b7aa3a5a22ab8d86
Myseie <123580372+Myseie@users.noreply.github.com>
2024-09-06 09:36:45 +0200

    Add files via upload

---
+++ Vowels counter/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Skriv en mening här : ");
+string input = Console.ReadLine();
+
+int vowels = 0;
+int consonants = 0;
+
+foreach (char tecken in input)
+{
+    char lowerChar = char.ToLower(tecken);
+
+    if ("aeiou".Contains(lowerChar))
+    {
+        vowels++;
+    }
+    else if(char.IsLetter(lowerChar)) 
+    {
+        consonants++;
+    }
+}
+
+Console.WriteLine("Antalet vokaler : " + vowels);
+Console.WriteLine("Antalet Konsonanter : " +  consonants);
